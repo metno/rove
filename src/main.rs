f37ec0fda68f4500d8a86699fda7e1ab184a2f9a
Ingrid Rebecca Abraham <ingridra@met.no>
2022-05-31 13:18:54 +0200

    implement naive spoofed version of ValidateOne endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use coordinator::coordinator_server::{Coordinator, CoordinatorServer};
 use coordinator::{ValidateOneRequest, ValidateResponse};
 use futures::Stream;
-use std::pin::Pin;
+use std::{pin::Pin, time::Duration};
+use tokio::sync::mpsc;
+use tokio_stream::{wrappers::ReceiverStream, StreamExt};
 use tonic::{transport::Server, Request, Response, Status};
 
 pub mod coordinator {
@@ -19,10 +21,53 @@ impl Coordinator for MyCoordinator {
 
     async fn validate_one(
         &self,
-        _req: Request<ValidateOneRequest>,
+        req: Request<ValidateOneRequest>,
     ) -> Result<Response<Self::ValidateOneStream>, Status> {
-        //
-        Err(Status::unimplemented("not implemented"))
+        let inner_req = req.into_inner();
+
+        let mut stream = Box::pin(
+            tokio_stream::iter(vec![
+                ValidateResponse {
+                    data_id: inner_req.data_id,
+                    flag_id: 1,
+                    flag: 0,
+                },
+                ValidateResponse {
+                    data_id: inner_req.data_id,
+                    flag_id: 2,
+                    flag: 0,
+                },
+                ValidateResponse {
+                    data_id: inner_req.data_id,
+                    flag_id: 3,
+                    flag: 1,
+                },
+            ])
+            .throttle(Duration::from_millis(200)),
+        );
+
+        // spawn and channel are required if you want handle "disconnect" functionality
+        // the `out_stream` will not be polled after client disconnect
+        let (tx, rx) = mpsc::channel(128);
+        tokio::spawn(async move {
+            while let Some(item) = stream.next().await {
+                match tx.send(Result::<_, Status>::Ok(item)).await {
+                    Ok(_) => {
+                        // item (server response) was queued to be send to client
+                    }
+                    Err(_item) => {
+                        // output_stream was build from rx and both are dropped
+                        break;
+                    }
+                }
+            }
+            println!("\tclient disconnected");
+        });
+
+        let output_stream = ReceiverStream::new(rx);
+        Ok(Response::new(
+            Box::pin(output_stream) as Self::ValidateOneStream
+        ))
     }
 }
 
