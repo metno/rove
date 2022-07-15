fa20ec8b4b8e3a8fa85175ce32566189b5b8cb57
Ingrid Rebecca Abraham <ingridra@met.no>
2022-07-15 11:28:55 +0200

    stub grpc server

--- cmd/coordinator/main.go
+++ cmd/coordinator/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	pb "github.com/metno/rove/proto"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+type server struct {
+	pb.UnimplementedCoordinatorServer
+}
+
+func (s *server) ValidateOne(in *pb.ValidateOneRequest, srv pb.Coordinator_ValidateOneServer) error {
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 50051))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterCoordinatorServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
