13f11ea53fe43f5403148f36db0e2dab17360664
Ingrid <git@ingrids.email>
2022-10-28 11:08:53 +0200

    automatically convert result enum to int for grpc resp

--- src/bin/runner/main.rs
+++ src/bin/runner/main.rs
@@ -1,7 +1,7 @@
 use rove::cache;
 use runner::runner_server::{Runner, RunnerServer};
 use runner::{RunTestRequest, RunTestResponse};
-use titanlib_rs::qc_tests::{dip_check, DipResult};
+use titanlib_rs::qc_tests::dip_check;
 use tonic::{transport::Server, Request, Response, Status};
 
 pub mod runner {
@@ -22,13 +22,10 @@ impl Runner for MyRunner {
         let data = cache::get_timeseries_data(1, std::time::SystemTime::now()); //TODO use actual arguments
         let result = dip_check(data, 2., 3.); // TODO load actual params
 
-        let flag = match result {
-            // TODO derived integer conversion?
-            DipResult::Pass => 0,
-            _ => 1,
+        let response = RunTestResponse {
+            flag: result as u32,
+            flag_id: 0,
         };
-
-        let response = RunTestResponse { flag, flag_id: 0 };
 
         Ok(Response::new(response))
     }
