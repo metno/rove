67e33a09a9a0795d484942017918c6813cb51f48
Ingrid <git@ingrids.email>
2023-02-24 12:56:20 +0100

    cache: remove unneccessary return

--- src/cache.rs
+++ src/cache.rs
@@ -7,7 +7,7 @@ pub async fn get_timeseries_data(
 ) -> Result<[f32; 3], Box<dyn std::error::Error>> {
     let _resp = reqwest::get("https://frost-beta.met.no/api/v1/obs/met.no/filter/get?elementids=air_temperature&stationids=18700&incobs=true&time=latest").await?.json::<HashMap<String, String>>().await?;
 
-    return Ok([1., 1., 1.]); // TODO get actual data
+    Ok([1., 1., 1.]) // TODO get actual data
 }
 
 pub fn get_spatial_data(_station_id: u32, _unix_timestamp: i64) -> Points {
