d4fed0df499f76c6d930a06a1a5c87cbc15e1067
Ingrid <git@ingrids.email>
2023-03-23 16:26:17 +0100

    update protos to reflect API changes

--- build.rs
+++ build.rs
@@ -1,8 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/runner/runner.proto")?;
-    tonic_build::compile_protos("proto/coordinator/coordinator.proto")?;
-    // tonic_build::configure()
-    //     .out_dir("src/coordinator")
-    //     .compile(&["proto/coordinator.proto"], &["proto"])?;
+    tonic_build::compile_protos("proto/util.proto")?;
+    tonic_build::compile_protos("proto/runner.proto")?;
+    tonic_build::compile_protos("proto/coordinator.proto")?;
     Ok(())
 }
