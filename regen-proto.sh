0a6a0ab973233776c61eb134a982bbbce0b17913
Ingrid <git@ingrids.email>
2023-03-27 11:29:24 +0200

    remove obsolete golang components

--- regen-proto.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-protoc --go_out=./golang --go_opt=paths=source_relative --go-grpc_out=./golang --go-grpc_opt=paths=source_relative proto/coordinator/coordinator.proto
-protoc --go_out=./golang --go_opt=paths=source_relative --go-grpc_out=./golang --go-grpc_opt=paths=source_relative proto/runner/runner.proto
