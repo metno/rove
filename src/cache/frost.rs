4307eb8cf4cf2c6f81cc42cd60d10e729812989c
Ingrid Rebecca Abraham <ingridra@met.no>
2023-06-22 14:43:59 +0200

    cache: factor frost code into its own module

--- src/cache/frost.rs
+++ src/cache/frost.rs
@@ -1,21 +1,10 @@
 use chrono::prelude::*;
-use olympian::points::Points;
 use serde::{de::Error, Deserialize, Deserializer};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 #[non_exhaustive]
-pub enum CacheError {
-    #[error("series id `{0}` could not be parsed")]
-    InvalidSeriesId(String),
-    #[error("data source `{0}` not registered")]
-    InvalidDataSource(String),
-    #[error("frost connector failed")]
-    Frost(#[from] FrostError),
-}
-
-#[derive(Error, Debug)]
-#[non_exhaustive]
+// TODO: should we rename these to just Error since they're already scoped?
 pub enum FrostError {
     #[error("data id `{0}` could not be parsed")]
     InvalidDataId(String),
@@ -51,23 +40,6 @@ where
 }
 
 pub async fn get_timeseries_data(
-    series_id: String,
-    unix_timestamp: i64,
-) -> Result<[f32; 3], CacheError> {
-    let (data_source, data_id) = series_id
-        .split_once(':')
-        .ok_or(CacheError::InvalidSeriesId(series_id.clone()))?;
-
-    // TODO: find a more flexible and elegant way of handling this
-    match data_source {
-        "frost" => get_timeseries_data_frost(data_id, unix_timestamp)
-            .await
-            .map_err(CacheError::Frost),
-        _ => Err(CacheError::InvalidDataSource(data_source.to_string())),
-    }
-}
-
-pub async fn get_timeseries_data_frost(
     data_id: &str,
     unix_timestamp: i64,
 ) -> Result<[f32; 3], FrostError> {
@@ -171,8 +143,4 @@ pub async fn get_timeseries_data_frost(
     );
 
     Ok([1., 1., 1.]) // TODO get actual data
-}
-
-pub fn get_spatial_data(_station_id: u32, _unix_timestamp: i64) -> Points {
-    todo!()
 }
---
+++ src/cache/mod.rs
@@ -0,0 +1,37 @@
+use olympian::points::Points;
+use thiserror::Error;
+
+mod frost;
+
+#[derive(Error, Debug)]
+#[non_exhaustive]
+// TODO: should we rename these to just Error since they're already scoped?
+pub enum CacheError {
+    #[error("series id `{0}` could not be parsed")]
+    InvalidSeriesId(String),
+    #[error("data source `{0}` not registered")]
+    InvalidDataSource(String),
+    #[error("frost connector failed")]
+    Frost(#[from] frost::FrostError),
+}
+
+pub async fn get_timeseries_data(
+    series_id: String,
+    unix_timestamp: i64,
+) -> Result<[f32; 3], CacheError> {
+    let (data_source, data_id) = series_id
+        .split_once(':')
+        .ok_or(CacheError::InvalidSeriesId(series_id.clone()))?;
+
+    // TODO: find a more flexible and elegant way of handling this
+    match data_source {
+        "frost" => frost::get_timeseries_data(data_id, unix_timestamp)
+            .await
+            .map_err(CacheError::Frost),
+        _ => Err(CacheError::InvalidDataSource(data_source.to_string())),
+    }
+}
+
+pub fn get_spatial_data(_station_id: u32, _unix_timestamp: i64) -> Points {
+    todo!()
+}
