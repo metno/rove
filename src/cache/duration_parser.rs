e8b18113b584ebe0b5b9c7ea2a4c9005382325cd
Ingrid <git@ingrids.email>
2023-06-28 13:12:38 +0200

    frost: break date part of duration into own parser

--- src/cache/duration_parser.rs
+++ src/cache/duration_parser.rs
@@ -11,24 +11,31 @@ pub struct RelDuration {
     months: u32,
 }
 
-pub fn parse_duration(input: &str) -> IResult<&str, RelDuration> {
-    let (input, _) = tag("P")(input)?;
-
-    let (input, (years, months, _days)) = tuple((
+fn parse_date(input: &str) -> IResult<&str, (u32, u32, u32)> {
+    let (input, (years, months, days)) = tuple((
         opt(terminated(nom::character::complete::u32, tag("Y"))),
         opt(terminated(nom::character::complete::u32, tag("M"))),
         opt(terminated(nom::character::complete::u32, tag("D"))),
     ))(input)?;
 
-    println!("{}", input);
-
     Ok((
         input,
-        RelDuration {
-            years: years.unwrap_or_default(),
-            months: months.unwrap_or_default(),
-        },
+        (
+            years.unwrap_or_default(),
+            months.unwrap_or_default(),
+            days.unwrap_or_default(),
+        ),
     ))
+}
+
+pub fn parse_duration(input: &str) -> IResult<&str, RelDuration> {
+    let (input, _) = tag("P")(input)?;
+
+    let (input, (years, months, _days)) = parse_date(input)?;
+
+    println!("{}", input);
+
+    Ok((input, RelDuration { years, months }))
 }
 
 #[cfg(test)]
