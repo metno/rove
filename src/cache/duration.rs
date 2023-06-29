a61567a7b2fa40cdb8bd10842d61736d5b9a0808
Ingrid <git@ingrids.email>
2023-06-29 15:08:17 +0200

    cache: parse time component of duration

--- src/cache/duration.rs
+++ src/cache/duration.rs
@@ -7,6 +7,10 @@ use nom::{
     IResult,
 };
 
+fn dhms_to_duration(days: i32, hours: i32, minutes: i32, seconds: i32) -> Duration {
+    Duration::seconds((((days * 24 + hours) * 60 + minutes) * 60 + seconds) as i64)
+}
+
 fn parse_date(input: &str) -> IResult<&str, (i32, i32, i32)> {
     let (input, (years, months, days)) = tuple((
         opt(terminated(nom::character::complete::i32, tag("Y"))),
@@ -24,16 +28,38 @@ fn parse_date(input: &str) -> IResult<&str, (i32, i32, i32)> {
     ))
 }
 
+fn parse_time(input: &str) -> IResult<&str, (i32, i32, i32)> {
+    let (input, (hours, minutes, seconds)) = tuple((
+        opt(terminated(nom::character::complete::i32, tag("H"))),
+        opt(terminated(nom::character::complete::i32, tag("M"))),
+        opt(terminated(nom::character::complete::i32, tag("S"))),
+    ))(input)?;
+
+    Ok((
+        input,
+        (
+            hours.unwrap_or_default(),
+            minutes.unwrap_or_default(),
+            seconds.unwrap_or_default(),
+        ),
+    ))
+}
+
 pub fn parse_duration(input: &str) -> IResult<&str, RelativeDuration> {
     let (input, _) = tag("P")(input)?;
 
-    let (input, (years, months, _days)) = parse_date(input)?;
+    let (input, (years, months, days)) = parse_date(input)?;
+
+    let (input, _) = tag("T")(input)?;
+
+    let (input, (hours, minutes, seconds)) = parse_time(input)?;
 
     println!("{}", input);
 
     Ok((
         input,
-        RelativeDuration::months(years * 12 + months).with_duration(Duration::seconds(0)),
+        RelativeDuration::months(years * 12 + months)
+            .with_duration(dhms_to_duration(days, hours, minutes, seconds)),
     ))
 }
 
@@ -46,8 +72,8 @@ mod tests {
         assert_eq!(
             parse_duration("P1YT1S"),
             Ok((
-                "T1S",
-                RelativeDuration::months(12).with_duration(Duration::seconds(0)),
+                "",
+                RelativeDuration::months(12).with_duration(Duration::seconds(1)),
             )),
         );
     }
