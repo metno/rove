7dd7721980851cc17368037f34f04dd53a3659fc
Ingrid <git@ingrids.email>
2023-07-13 15:57:02 +0200

    use ; as delimiter

--- rove/src/data_switch/lustre_netatmo.rs
+++ rove/src/data_switch/lustre_netatmo.rs
@@ -4,7 +4,7 @@ use std::fs::File;
 pub fn read_netatmo() {
     let file = File::open("/lustre/storeB/immutable/archive/projects/metproduction/yr_short/2023/07/13/obs_ta_20230713T00Z.txt").unwrap();
 
-    let mut rdr = csv::Reader::from_reader(file);
+    let mut rdr = csv::ReaderBuilder::new().delimiter(b';').from_reader(file);
     for record_res in rdr.records() {
         let record = record_res.unwrap();
         println!("{:?}", record);
