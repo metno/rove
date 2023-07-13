8d91560ac7c2f26b3d4361cf356e305cf330dcc9
Ingrid <git@ingrids.email>
2023-07-13 16:10:45 +0200

    take timestamp param, and use it to construct correct file path

--- rove/src/data_switch/lustre_netatmo.rs
+++ rove/src/data_switch/lustre_netatmo.rs
@@ -1,8 +1,16 @@
+use crate::util::Timestamp;
+use chrono::prelude::*;
 use csv;
 use std::fs::File;
 
-pub fn read_netatmo() {
-    let file = File::open("/lustre/storeB/immutable/archive/projects/metproduction/yr_short/2023/07/13/obs_ta_20230713T00Z.txt").unwrap();
+pub fn read_netatmo(time: Timestamp) {
+    let time = Utc.timestamp_opt(time.0, 0).unwrap();
+
+    // TODO: assert minute and second are both 0
+
+    let path = format!("{}", time.format("/lustre/storeB/immutable/archive/projects/metproduction/yr_short/%Y/%m/%d/obs_ta_%Y%m%dT%HZ.txt"));
+
+    let file = File::open(path).unwrap();
 
     let mut rdr = csv::ReaderBuilder::new().delimiter(b';').from_reader(file);
     for record_res in rdr.records() {
@@ -17,6 +25,10 @@ mod tests {
 
     #[test]
     fn test_read_netatmo() {
-        read_netatmo();
+        read_netatmo(Timestamp(
+            Utc.with_ymd_and_hms(2023, 07, 13, 0, 0, 0)
+                .unwrap()
+                .timestamp(),
+        ));
     }
 }
