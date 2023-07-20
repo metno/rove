5678f2630bca0f5c779fc001cd91eee022d16afc
Ingrid <git@ingrids.email>
2023-07-20 16:51:39 +0200

    lustre_netatmo: format csv data into SpatialCache correctly

--- lustre_netatmo/src/lib.rs
+++ lustre_netatmo/src/lib.rs
@@ -16,7 +16,11 @@ struct Record {
     lon: f32,
     elev: f32,
     value: f32,
+    // Provider ID
+    // 1=WMO stations, 2=MET Non-WMO stations, 3=Netatmo, 4=Foreign WMO, 5=SVV, 6=Bergensværet, 7=FMI, 8=Luftambulansen, 9=Holfuy, 100=Radar precipitation
     prid: u32,
+    // QC flag
+    // 0 = OK, >=l = fail
     dqc: u32,
 }
 
@@ -29,14 +33,28 @@ fn read_netatmo(time: Timestamp) -> Result<SpatialCache, data_switch::Error> {
 
     let file = File::open(path).unwrap();
 
+    // TODO: probably some optimisation potential here?
+    let mut lats = Vec::new();
+    let mut lons = Vec::new();
+    let mut elevs = Vec::new();
+    let mut values = Vec::new();
+
     let mut rdr = csv::ReaderBuilder::new().delimiter(b';').from_reader(file);
     for result in rdr.deserialize() {
         let record: Record = result.unwrap();
-        println!("{:?}", record);
+
+        // TODO: should we allow more prids?
+        // prid 3 represents netatmo data, but if we use this as a backing set
+        // I wonder if there's any harm in adding others
+        if record.prid == 3 && record.dqc == 0 {
+            lats.push(record.lat);
+            lons.push(record.lon);
+            elevs.push(record.elev);
+            values.push(record.value);
+        }
     }
 
-    // TODO: actually populate these vecs
-    Ok(SpatialCache::new(vec![], vec![], vec![], vec![]))
+    Ok(SpatialCache::new(lats, lons, elevs, values))
 }
 
 #[async_trait]
