2b00d1a28481e4c0a850b066263cdec184e57dd7
Ingrid <git@ingrids.email>
2023-07-20 17:28:45 +0200

    use lustre_netatmo DataSource in example_binary

--- example_binary/src/main.rs
+++ example_binary/src/main.rs
@@ -1,14 +1,18 @@
 use frost::Frost;
+use lustre_netatmo::LustreNetatmo;
 use rove::{
     coordinator::{start_server, ListenerType},
     data_switch::{DataSource, DataSwitch},
 };
 use std::collections::HashMap;
 
-// TODO: use anyhow for error handling
+// TODO: use anyhow for error handling?
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let data_switch = DataSwitch::new(HashMap::from([("frost", &Frost as &dyn DataSource)]));
+    let data_switch = DataSwitch::new(HashMap::from([
+        ("frost", &Frost as &dyn DataSource),
+        ("lustre_netatmo", &LustreNetatmo as &dyn DataSource),
+    ]));
 
     start_server(ListenerType::Addr("[::1]:1337".parse()?), data_switch).await
 }
