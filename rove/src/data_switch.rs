25238c0f280509f2bd3f5adaedb08aa97bb7e711
Ingrid <git@ingrids.email>
2023-07-20 16:04:38 +0200

    rove/data_switch: refine SpatialCache type def and provide a constructor

--- rove/src/data_switch.rs
+++ rove/src/data_switch.rs
@@ -1,6 +1,6 @@
 use async_trait::async_trait;
 use chronoutil::RelativeDuration;
-use olympian::points::Points;
+use olympian::points::{CoordinateType, Points};
 use std::collections::HashMap;
 use thiserror::Error;
 
@@ -34,7 +34,20 @@ pub struct SeriesCache {
 }
 
 pub struct SpatialCache {
-    pub data: Points,
+    pub rtree: Points,
+    pub data: Vec<f32>,
+}
+
+impl SpatialCache {
+    pub fn new(lats: Vec<f32>, lons: Vec<f32>, elevs: Vec<f32>, values: Vec<f32>) -> Self {
+        // TODO: ensure vecs have same size
+        // TODO: figure out what to do about lafs and ctype
+        let n = values.len();
+        Self {
+            rtree: Points::from_latlons(lats, lons, elevs, vec![0.; n], CoordinateType::Cartesian),
+            data: values,
+        }
+    }
 }
 
 #[async_trait]
