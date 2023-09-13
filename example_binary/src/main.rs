accde70a44af3ed96d300a7f40b560845afd738b
Ingrid <git@ingrids.email>
2023-09-13 13:33:27 +0200

    example_binary: use clap to parse addr and trace level

--- example_binary/src/main.rs
+++ example_binary/src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use example_binary::construct_hardcoded_dag;
 use frost::Frost;
 use lustre_netatmo::LustreNetatmo;
@@ -6,17 +7,33 @@ use rove::{
     server::{start_server, ListenerType},
 };
 use std::collections::HashMap;
+use tracing::Level;
+
+// TODO: add author/version/about metadata?
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long, default_value_t = String::from("[::1]:1337"))]
+    address: String,
+    #[arg(short = 'l', long, default_value_t = Level::INFO)]
+    max_trace_level: Level,
+}
 
 // TODO: use anyhow for error handling?
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    tracing_subscriber::fmt()
+        .with_max_level(args.max_trace_level)
+        .init();
+
     let data_switch = DataSwitch::new(HashMap::from([
         ("frost", &Frost as &dyn DataSource),
         ("lustre_netatmo", &LustreNetatmo as &dyn DataSource),
     ]));
 
     start_server(
-        ListenerType::Addr("[::1]:1337".parse()?),
+        ListenerType::Addr(args.address.parse()?),
         data_switch,
         construct_hardcoded_dag(),
     )
