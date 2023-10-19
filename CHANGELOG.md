8f93fe5786ff479ad8207893a41cbcc7f9cfde1c
Ingrid <git@ingrids.email>
2023-10-19 17:50:07 +0200

    add flag when compiling protos to fix docs.rs compatibility

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## 0.1.1
+
+**bugfixes:**
+
+- added flag `--experimental_allow_proto3_optional` when compiling protos, to fix compatibility with docs.rs and its old protoc
--- build.rs
+++ build.rs
@@ -1,4 +1,9 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/rove.proto")?;
+    // tonic_build::compile_protos("proto/rove.proto")?;
+    // needed the extra flag to make docs.rs happy :(. we can probably switch
+    // back to the commented version once they update their protoc
+    tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile(&["proto/rove.proto"], &["proto"])?;
     Ok(())
 }
