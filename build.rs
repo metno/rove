89bdd67db4395bf8b2650b67781336159ab46d8a
Louise Oram <louiseo@met.no>
2025-11-03 22:19:17 +0100

    upgrade prost and tonic

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // tonic_build::compile_protos("proto/rove.proto")?;
     // needed the extra flag to make docs.rs happy :(. we can probably switch
     // back to the commented version once they update their protoc
-    tonic_build::configure()
+    tonic_prost_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(&["proto/rove.proto"], &["proto"])?;
     Ok(())
